06d25e304936214a9fd76d5fcbecf0e7432e9592
Gansca Paul Mihai <gansca.paul@gmail.com>
2018-07-29 13:13:56 +0300

    Project structure setup

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) =>
+    res.send(`Node and express server running on port ${PORT}`)
+);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
